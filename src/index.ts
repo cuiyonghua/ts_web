a41b7ce5e8adad875fa926aff95c433c0e3d8964
NVIDIA.COM\ycui <ycui@nvidia.com>
2019-11-27 16:49:45 +0800

    add save and fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import { User } from './models/User';
 
-const user = new User({name: 'myname', age: 20});
+// const user = new User({name: 'myname', age: 20});
 
-user.on('change', () => {
-    console.log('Change #1');
-});
-user.on('change', () => {
-    console.log('Change #2');
-});
-user.on('save', () => {
-    console.log('Save was triggered');
-});
+// user.on('change', () => {
+//     console.log('Change #1');
+// });
+// user.on('change', () => {
+//     console.log('Change #2');
+// });
+// user.on('save', () => {
+//     console.log('Save was triggered');
+// });
 
-user.trigger('sasas');
+// user.trigger('sasas');
 
 // console.log(user);
+
+
+// const user = new User({ id: 1});
+
+// user.fetch();
+
+// setTimeout(()=> {
+//     console.log(user);
+// }, 4000);
+
+
+const user = new User({ name: 'New update', age: 56565, id: 3});
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,8 @@
+import axios, { AxiosResponse } from 'axios';
+
 
 interface UserProps {
+    id?: number;
     name?: string;
     age?: number;
 }
@@ -36,6 +39,24 @@ export class User {
             callback();
         });
 
+    }
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`).then( 
+            (response: AxiosResponse): void => {
+                this.set(response.data);
+            }
+        );
+    }
+
+    save(): void {
+        const id = this.get('id');
+
+        if(id) {
+            axios.put(`http://localhost:3000/users/${this.get('id')}`, this.data);
+        } else {
+            axios.post('http://localhost:3000/users', this.data);
+        }
     }
 
 }
